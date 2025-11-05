eabdffbb4b3fb59c16d59f94029b87ec5a7072cf
hanzo <hanzo@debian>
2025-11-04 21:14:34 -0600

    Primer commit

---
+++ src/modelo/ModeloCliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author hanzo
+ */
+public class ModeloCliente {
+    // atributos
+    private String nombres;
+    private String apellidos;
+    
+    
+}
