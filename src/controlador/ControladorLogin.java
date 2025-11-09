ca0253c508a5c2b3dd61c682fe7043646b255efa
diamondqueso <dq3270@gmail.com>
2025-11-08 20:10:31 -0600

    Agregar módulo de autenticación y seguridad en ControladorLogin

--- src/controlador/ControladorLogin.java
+++ src/controlador/ControladorLogin.java
@@ -9,7 +9,44 @@ package controlador;
  * @author hanzo
  */
 public class ControladorLogin {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    /*
+     * 1. Módulo de Autenticación y Seguridad
+     * Permite el acceso seguro al sistema mediante usuarios registrados. Incluye formulario de login y registro con validaciones.
+     * 
+     * Validaciones:
+     * · - El email y nombre no deben repetirse.
+     * · - Las contraseñas deben coincidir.
+     */
+
+    // Datos del usuario
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String telefono;
+    private String genero;
+    private String email;
+    private String contrasena;
+
+    public ControladorLogin(String nombre, String apellido, String dni, String telefono, String genero, String email,
+            String contrasena) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.genero = genero;
+        this.email = email;
+        this.contrasena = contrasena;
     }
+    
+    //Getters
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public String getDni() { return dni; }
+    public String getTelefono() { return telefono; }
+    public String getGenero() { return genero; }
+    public String getEmail() { return email; }
+    public String getContrasena() { return contrasena; }
+    
+    //Setters
+    public void setNombre(String nombre) { this.nombre = nombre; }
 }
